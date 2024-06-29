44a7f33de3cf9748abacc7737338ae2fbba48d7e
lehaifeng <lehaifeng000@163.com>
2024-06-29 08:32:52 +0800

    Initial Commit

---
+++ MemoryGridUITests/MemoryGridUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryGridUITestsLaunchTests.swift
+//  MemoryGridUITests
+//
+//  Created by 乐海丰 on 2024/6/29.
+//
+
+import XCTest
+
+final class MemoryGridUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
